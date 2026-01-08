f26b575729d675a2febeca4cdf0627c717babf98
mghniox <mghniox@gmail.com>
2026-01-08 21:17:23 +0700

    initial setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='text-sm text-center italic text-gray-400'>&copy; 2026 Source AI Vision</footer>
+  )
+}
+
+export default Footer
